9968f2a55e604bea60952987b0e1f53c4675351b
MjChen120 <156042164+MjChen120@users.noreply.github.com>
2024-02-19 18:33:20 -0500

    01-download_data.R

---
+++ scripts/01-download_data.R
@@ -0,0 +1,2 @@
+# The download of the raw data requires sign-in to Open Science Framework (OSF) website (https://osf.io/d2e6s/). 
+# The whole replication package is avaliable on site for downloading.
